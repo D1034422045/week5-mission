dcf18e6bf3c2ab0c331db929891948fc5829980e
倫倫 <f5badapple@gmail.com>
2025-02-15 22:20:18 +0800

    第六週主線任務

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
 
 import "bootstrap";
 import "./assets/all.scss";
+import router from "./router";
 
-import App from "./App.jsx";
+// import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}>{/* <App /> */}</RouterProvider>
   </StrictMode>
 );
